4d67e1a301c0288f28ce09fd8ad39d2a95adf536
Anwar Madani <15781928+AnwarMadani@users.noreply.github.com>
2024-11-06 13:46:18 +0100

    add logic to add and remove tasks

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ var yyyy = today.getFullYear();
 
 today = dd + '/' + mm + '/' + yyyy;
 
-const addProjectBtn = document.getElementById('addProject');
+let myTasks = [];
+const addTaskBtn = document.getElementById('addTask');
 const modal = document.getElementById('modal');
 const overlay = document.getElementById('overlay');
 const form = document.getElementById('form');
 const datePicker = document.getElementById('dueDate');
-datePicker.max = yyyy + '/' + mm + '/' + dd;
+datePicker.min = yyyy + '/' + mm + '/' + dd;
 const container = document.getElementById('container');
-let tasks = [];
-addProjectBtn.addEventListener("click", openModal);
 
+addTaskBtn.addEventListener("click", openModal);
+overlay.addEventListener("click", closeModal);
 
 function openModal(){
     form.reset();
@@ -22,18 +23,54 @@ function openModal(){
     overlay.classList.add('active');
 }
 
-overlay.addEventListener("click", closeModal);
-
 function closeModal(){
     modal.classList.remove('active');
     overlay.classList.remove('active');
 }
 
-form.addEventListener("submit",(e) => addTask(e));
 
-function addTask(e){
-    e.preventDefault();
+class Task {
+    constructor(title, dueDate, priority, description=''){
+        this.title = title,
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+    
+    getName() {
+        return this.name
+    }
+    
+    
+    getDate() {
+        return this.dueDate
+    }
 
+}
+
+function addTaskToTasks(newTask){
+    if(!myTasks.includes(newTask)){
+        myTasks.push(newTask);
+    }
+}
+
+function removeTaskFromTasks(title){
+    myTasks = myTasks.filter(task => task.title !== title)
+}
+
+function getTask(title){
+    return myTasks.find(task => task.title === title)
+}
+
+
+function updateTasksContainer(){
+    container.innerHTML = '';
+    for(let task of myTasks){
+        createTaskCard(task);
+    }
+}
+
+function createTask(){
     const title = document.getElementById('title').value;
     const dueDate = document.getElementById('dueDate').value;
     let description = document.getElementById('description').value;
@@ -47,161 +84,54 @@ function addTask(e){
     }
 
     const newTask = new Task(title, dueDate, priority, description);
-    console.log(newTask);
-    tasks.push(newTask);
+    return newTask;
+}
 
-    updateTasksUi();
+function createTaskCard(task){
+        const taskDiv = document.createElement('div');
+        const leftSideDiv = document.createElement('div');
+        const deleteIcon = document.createElement('img');
+        deleteIcon.src = './img/delete.png';
+        taskDiv.classList.add('task');
+        const taskTitle = document.createElement('h3');
+        taskTitle.textContent = task.title;
+        const taskDueDate = document.createElement('p');
+        taskDueDate.textContent = task.dueDate;
+        const firstDiv = document.createElement('div');
+        firstDiv.classList.add('first');
+        const taskDescription = document.createElement('p');
+        const descriptionDiv = document.createElement('div');
+        taskDescription.textContent = task.description;
+
+        deleteIcon.addEventListener("click", (e) => deleteTask(e))
+        
+        descriptionDiv.appendChild(taskDescription);
+        leftSideDiv.appendChild(deleteIcon);
+        leftSideDiv.appendChild(taskTitle);
+        firstDiv.appendChild(leftSideDiv);
+
+        taskDiv.classList.add(task.priority)
+        firstDiv.appendChild(taskDueDate);
+        taskDiv.appendChild(firstDiv);
+        taskDiv.appendChild(descriptionDiv)
+        container.appendChild(taskDiv);
+}
+
+form.addEventListener("submit", (e) => addTask(e));
+
+function deleteTask(e){
+    let title = e.target.parentNode.parentNode.firstChild.innerHTML;
+    title = title.split('<h3>');
+    title = title[1].split('<');
+    title = title[0];
+    removeTaskFromTasks(title);
+    updateTasksContainer();
+}
+
+function addTask(e){
+    e.preventDefault();
+    const newTask = createTask()
+    addTaskToTasks(newTask);
+    updateTasksContainer();
     closeModal();
 }
-
-function updateTasksUi(){
-    container.innerHTML = '';
-    tasks.forEach((task) => {
-        if(task.description === ''){
-            const taskDiv = document.createElement('div');
-            taskDiv.classList.add('task');
-            const taskTitle = document.createElement('h3');
-            taskTitle.textContent = task.title;
-            const taskDueDate = document.createElement('p');
-            taskDueDate.textContent = task.dueDate;
-            const firstDiv = document.createElement('div');
-            firstDiv.classList.add('first');
-            taskDiv.classList.add(task.priority);
-
-            firstDiv.appendChild(taskTitle);
-            firstDiv.appendChild(taskDueDate);
-            taskDiv.appendChild(firstDiv)
-            container.appendChild(taskDiv);
-        } else {
-            const taskDiv = document.createElement('div');
-            taskDiv.classList.add('task');
-            const taskTitle = document.createElement('h3');
-            taskTitle.textContent = task.title;
-            const taskDueDate = document.createElement('p');
-            taskDueDate.textContent = task.dueDate;
-            const firstDiv = document.createElement('div');
-            firstDiv.classList.add('first');
-            const taskDescription = document.createElement('p');
-            const descriptionDiv = document.createElement('div');
-            taskDescription.textContent = task.description;
-            descriptionDiv.appendChild(taskDescription);
-
-            taskDiv.classList.add(task.priority)
-            firstDiv.appendChild(taskTitle);
-            firstDiv.appendChild(taskDueDate);
-            taskDiv.appendChild(firstDiv);
-            taskDiv.appendChild(descriptionDiv)
-            container.appendChild(taskDiv);
-        }
-        
-    })
-
-}
-
-class Task {
-    constructor(title, dueDate, priority, description=''){
-        this.title = title,
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-
-    setName(name) {
-        this.name = name
-    }
-    
-    getName() {
-        return this.name
-    }
-    
-    setDate(dueDate) {
-        this.dueDate = dueDate
-    }
-    
-    getDate() {
-        return this.dueDate
-    }
-
-}
-
-class Project{
-    constructor(name){
-        this.name = name,
-        this.tasks = []
-    }
-
-    setName(name){
-        this.name = name
-    }
-
-    getName(){
-        return this.name
-    }
-
-    setTasks(tasks){
-        this.tasks = tasks
-    }
-
-    getTasks(){
-        return this.tasks
-    }
-
-    getTask(taskName){
-        return this.tasks.find((task) => task.getName() === taskName)
-    }
-
-    contains(taskName){
-        return this.tasks.some((task) => task.getName() === taskName)
-    }
-
-    addTask(newTask){
-        if(this.tasks.find((task) => task.getName() === newTask.name)) return
-        this.tasks.push(newTask)
-    }
-
-    deleteTask(taskName){
-        this.tasks = this.tasks.filter((task) => task.getName() !== taskName)
-    }
-
-    getTasksOfToday(){
-        return this.tasks.filter((task) => {
-            const taskDate = new Date(task.getDate());
-            return taskDate == today
-        })
-    }
-
-}
-
-class TodoList{
-    constructor(){
-        this.projects = []
-    }
-
-    setProjects(projects){
-        this.projects = projects
-    }
-
-    getProjects(){
-        return this.projects
-    }
-
-    getProject(projectName){
-        return this.projects.find((project) => project.getName() === projectName)
-    }
-
-    contains(projectName){
-        return this.projects.some((project) => project.getName() === projectName)
-    }
-
-    addProject(newProject){
-        if(this.projects.find((project) => project.name === newProject.name)) return
-        this.projects.push(newProject);
-    }
-
-    deleteProject(projectName){
-        const projectToDelete = this.projects.find((project) => project.getName() === projectName)
-        this.projects.splice(this.projects.indexOf(projectToDelete), 1);
-    }
-
-}
-
